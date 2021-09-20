a0b47499a2831793a542684d9df3f72372f4c0c6
Wajih Kammoun <wajih.km@gmail.com>
2021-09-20 13:46:15 +0400

    Fawry fix

--- admin/language/en-gb/extension/payment/paytabs_atfawry.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'PayTabs - @Fawry';
-
-// Text
-$_['text_paytabs_atfawry'] = '<img src="view/image/payment/paytabs/atfawry.png" alt="PayTabs - @Fawry" title="PayTabs - @Fawry" />';
---
+++ admin/language/en-gb/extension/payment/paytabs_fawry.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - @Fawry';
+
+// Text
+$_['text_paytabs_fawry'] = '<img src="view/image/payment/paytabs/fawry.png" alt="PayTabs - @Fawry" title="PayTabs - @Fawry" />';
