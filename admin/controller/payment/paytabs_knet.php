95828c8627c3fd70ce0ac824ce7fd9e3b9a75b19
Wajih <wajih.km@gmail.com>
2022-10-27 14:57:57 +0400

    Admin/Controller Introduce Abstract class Refactor the code

--- admin/controller/payment/paytabs_knet.php
+++ admin/controller/payment/paytabs_knet.php
@@ -2,53 +2,8 @@
 
 namespace Opencart\Admin\Controller\Extension\Paytabs\Payment;
 
-use Opencart\System\Library\PaytabsController;
 
 class PaytabsKnet extends \Opencart\System\Engine\Controller
 {
 	public $_code = 'knet';
-	public $error = array();
-	public $userToken;
-
-	private $paytabsController;
-
-	//
-
-	function init()
-	{
-		// $this->load->library('paytabs_api');
-        require_once DIR_EXTENSION . 'paytabs/system/library/paytabs_api.php';
-
-		$this->paytabsController = new PaytabsController($this);
-	}
-
-
-	public function index()
-	{
-		$this->init();
-
-		$this->paytabsController->index($data);
-	}
-
-
-	public function save()
-	{
-		$this->paytabsController->save();
-	}
-
-
-	/**
-	 * Validate Extension's settings before saving the new values
-	 */
-	public function validate()
-	{
-		return $this->paytabsController->validate();
-	}
-
-
-	public function install()
-	{
-		$this->init();
-		$this->paytabsController->install();
-	}
 }
