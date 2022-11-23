42e18cddfe1ee89e7bda00923999bab4fcfc5bf9
Wajih <wajih.km@gmail.com>
2022-11-23 17:31:58 +0400

    Touchpoints (#14)
    * Add TouchPoints method
    * SDK update

---
+++ admin/language/en-gb/payment/paytabs_touchpoints.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - TouchPoints';
+
+// Text
+$_images_url = HTTP_CATALOG . 'extension/paytabs/admin/view/image/';
+$_['text_paytabs_touchpoints'] = '<img src="' . $_images_url . 'touchpoints_adcb.svg" alt="PayTabs - TouchPoints" title="PayTabs - TouchPoints" style="max-width: 90px; max-height: 70px;" />';
--- catalog/language/en-gb/payment/paytabs_strings.php
+++ catalog/language/en-gb/payment/paytabs_strings.php
@@ -1,16 +1,17 @@
 <?php
 // Text
-$_['all_text_title']        = 'Online payments powered by PayTabs';
-$_['creditcard_text_title'] = 'PayTabs - CreditCard';
-$_['mada_text_title']       = 'PayTabs - Mada';
-$_['omannet_text_title']    = 'PayTabs - OmanNet';
-$_['amex_text_title']       = 'PayTabs - Amex';
-$_['knet_text_title']       = 'PayTabs - KnPay';
-$_['applepay_text_title']   = 'PayTabs - ApplePay';
-$_['fawry_text_title']      = 'PayTabs - @Fawry';
-$_['sadad_text_title']      = 'PayTabs - Sadad';
-$_['stcpay_text_title']     = 'PayTabs - StcPay';
-$_['valu_text_title']       = 'PayTabs - valU';
-$_['meeza_text_title']      = 'PayTabs - Meeza';
-$_['meezaqr_text_title']    = 'PayTabs - Meeza QR';
-$_['unionpay_text_title']   = 'PayTabs - UnionPay';
+$_['all_text_title']         = 'Online payments powered by PayTabs';
+$_['creditcard_text_title']  = 'PayTabs - CreditCard';
+$_['mada_text_title']        = 'PayTabs - Mada';
+$_['omannet_text_title']     = 'PayTabs - OmanNet';
+$_['amex_text_title']        = 'PayTabs - Amex';
+$_['knet_text_title']        = 'PayTabs - KnPay';
+$_['applepay_text_title']    = 'PayTabs - ApplePay';
+$_['fawry_text_title']       = 'PayTabs - @Fawry';
+$_['sadad_text_title']       = 'PayTabs - Sadad';
+$_['stcpay_text_title']      = 'PayTabs - StcPay';
+$_['valu_text_title']        = 'PayTabs - valU';
+$_['meeza_text_title']       = 'PayTabs - Meeza';
+$_['meezaqr_text_title']     = 'PayTabs - Meeza QR';
+$_['unionpay_text_title']    = 'PayTabs - UnionPay';
+$_['touchpoints_text_title'] = 'PayTabs - TouchPoints';
