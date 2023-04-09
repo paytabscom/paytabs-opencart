61a0c577803a9b7302a1644048219d4c3129df7c
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-04-10 01:24:35 +0200

    installment

---
+++ admin/controller/extension/payment/paytabs_installment.php
@@ -0,0 +1,47 @@
+class ControllerExtensionPaymentPaytabsInstallment extends Controller
+{
+	public $_code = 'installment';
+	public $error = array();
+	public $userToken;
+
+	private $paytabsController;
+
+	//
+
+	function init()
+	{
+		$this->load->library('paytabs_api');
+
+		$this->paytabsController = new PaytabsController($this);
+	}
+
+
+	public function index()
+	{
+		$this->init();
+
+		$this->paytabsController->index($data);
+	}
+
+
+	public function save()
+	{
+		$this->paytabsController->save();
+	}
+
+
+	/**
+	 * Validate Extension's settings before saving the new values
+	 */
+	public function validate()
+	{
+		return $this->paytabsController->validate();
+	}
+
+
+	public function install()
+	{
+		$this->init();
+		$this->paytabsController->install();
+	}
+}
