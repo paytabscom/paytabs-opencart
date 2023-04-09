873650d064af0a12875d421dfa218195b2b9cb9c
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-04-10 01:36:44 +0200

    urpay

---
+++ admin/language/en-gb/extension/payment/paytabs_urpay.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Urpay';
+
+// Text
+$_['text_paytabs_urpay'] = '<img src="view/image/payment/paytabs/urpay.svg" alt="PayTabs - Urpay" title="PayTabs - Urpay" style="max-width: 90px; max-height: 70px;" />';
