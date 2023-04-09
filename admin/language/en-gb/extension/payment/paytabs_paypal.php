0de5792da9ac5a18db44e0e13d66d7cfd9bf956b
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-04-10 01:14:08 +0200

    Paypal

---
+++ admin/language/en-gb/extension/payment/paytabs_paypal.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Paypal';
+
+// Text
+$_['text_paytabs_paypal'] = '<img src="view/image/payment/paytabs/paypal.svg" alt="PayTabs - Paypal" title="PayTabs - Paypal" style="max-width: 90px; max-height: 70px;" />';
