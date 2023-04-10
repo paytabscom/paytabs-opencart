c4cdc20a2fe48d71ef82f0017014903acee59d9f
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-04-10 02:29:01 +0200

    Installment

---
+++ catalog/model/payment/paytabs_installment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Opencart\Catalog\Model\Extension\Paytabs\Payment;
+
+require_once DIR_EXTENSION . 'paytabs/system/library/paytabs_api.php';
+
+use Opencart\System\Library\PaytabsCatalogModel;
+
+class PaytabsInstallment extends PaytabsCatalogModel
+{
+	public $_code = 'installment';
+}
+
