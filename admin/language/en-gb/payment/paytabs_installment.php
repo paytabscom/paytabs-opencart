6e854f3079f67008a84befca23db9087ee32ef6f
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-04-10 02:25:23 +0200

    Installment

---
+++ admin/language/en-gb/payment/paytabs_installment.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Installment';
+
+// Text
+$_images_url = HTTP_CATALOG . 'extension/paytabs/admin/view/image/';
+$_['text_paytabs_installment'] = '<img src="' . $_images_url . 'nbe-installment.png" alt="PayTabs - Installment" title="PayTabs - Installment" style="max-width: 90px; max-height: 70px;" />';
