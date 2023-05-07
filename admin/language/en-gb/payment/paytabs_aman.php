52748ceab0632eb75c1a23dbd5f3c6e3caf1d3fc
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-05-07 17:20:46 +0300

    add aman method

---
+++ admin/language/en-gb/payment/paytabs_aman.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Aman';
+
+// Text
+$_images_url = HTTP_CATALOG . 'extension/paytabs/admin/view/image/';
+$_['text_paytabs_aman'] = '<img src="' . $_images_url . 'aman.svg" alt="PayTabs - Aman" title="PayTabs - Aman" style="max-width: 90px; max-height: 70px;" />';
