34a2838479e1b487ba48009e18504c2ae4f96dda
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-09-18 02:54:12 +0300

    forsa payment

---
+++ admin/language/en-gb/extension/payment/paytabs_forsa.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Forsa';
+
+// Text
+$_['text_paytabs_forsa'] = '<img src="view/image/payment/paytabs/forsa.png" alt="PayTabs - Forsa" title="PayTabs - Forsa" style="max-width: 90px; max-height: 70px;" />';
