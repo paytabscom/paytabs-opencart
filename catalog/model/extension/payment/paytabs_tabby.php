ab7e6dd0ba2f995676af6d84b1a21ba47d2552a9
Walaa-Elsaeed-93 <w.elsaeed@paytabs.com>
2023-10-10 21:43:43 +0000

    tabby payment method

---
+++ catalog/model/extension/payment/paytabs_tabby.php
@@ -0,0 +1,24 @@
+<?php
+
+class ModelExtensionPaymentPaytabsTabby extends Model
+{
+	public $_code = 'tabby';
+
+	private $paytabsController;
+
+
+	private function init()
+	{
+		$this->load->library('paytabs_api');
+
+		$this->paytabsController = new PaytabsCatalogModel($this);
+	}
+
+
+	public function getMethod($address, $total)
+	{
+		$this->init();
+
+		return $this->paytabsController->getMethod($address, $total);
+	}
+}
