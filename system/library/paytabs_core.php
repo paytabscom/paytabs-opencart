ee7c2a9bce401e0b3406e3a574471cab2fea5634
Wajih <wajih.km@gmail.com>
2023-10-25 13:26:00 +0000

    Fix Tabby iFrame option

--- system/library/paytabs_core.php
+++ system/library/paytabs_core.php
@@ -2,11 +2,11 @@
 
 /**
  * PayTabs v2 PHP SDK
- * Version: 2.12.1
+ * Version: 2.12.2
  * PHP >= 7.0.0
  */
 
-define('PAYTABS_SDK_VERSION', '2.12.1');
+define('PAYTABS_SDK_VERSION', '2.12.2');
 
 define('PAYTABS_DEBUG_FILE_NAME', 'debug_paytabs.log');
 define('PAYTABS_DEBUG_SEVERITY', ['Info', 'Warning', 'Error']);
@@ -999,7 +999,7 @@ class PaytabsApi
         '21' => ['name' => 'installment', 'title' => 'PayTabs - Installment', 'currencies' => ['EGP'], 'groups' => [PaytabsApi::GROUP_CARDS, PaytabsApi::GROUP_IFRAME]],
         '22' => ['name' => 'touchpoints', 'title' => 'PayTabs - Touchpoints', 'currencies' => ['AED'], 'groups' => [PaytabsApi::GROUP_CARDS, PaytabsApi::GROUP_IFRAME]],
         '23' => ['name' => 'forsa', 'title' => 'PayTabs - Forsa', 'currencies' => ['EGP'], 'groups' => [PaytabsApi::GROUP_IFRAME]],
-        '24' => ['name' => 'tabby', 'title' => 'PayTabs - Tabby', 'currencies' => ['AED'], 'groups' => [PaytabsApi::GROUP_IFRAME]],
+        '24' => ['name' => 'tabby', 'title' => 'PayTabs - Tabby', 'currencies' => ['AED'], 'groups' => []],
 
     ];
 
@@ -1235,7 +1235,8 @@ class PaytabsApi
         }
 
         if (!$is_valid) {
-            PaytabsHelper::log("Paytabs Admin: Invalid Signature", 3);
+            $hashed_key = explode('-', @$this->server_key ?? '')[0];
+            PaytabsHelper::log("Paytabs Admin: Invalid Signature ({$hashed_key}, {$signature})", 3);
             return false;
         }
 
