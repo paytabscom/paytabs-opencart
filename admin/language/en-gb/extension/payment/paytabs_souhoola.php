bffdbf0efa185568cfd52602ae2829e1038c5127
Walaa-Elsaeed-93 <w.elsaeed@paytabs.com>
2023-11-07 03:20:46 +0200

    add souhoola paymnt method

---
+++ admin/language/en-gb/extension/payment/paytabs_souhoola.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Souhoola';
+
+// Text
+$_['text_paytabs_souhoola'] = '<img src="view/image/payment/paytabs/tabby.svg" alt="PayTabs - Tabby" title="PayTabs - Tabby" style="max-width: 90px; max-height: 70px;" />';
