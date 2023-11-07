e910514a586c77609c495a759a2ae85a9dac5acc
Walaa-Elsaeed-93 <w.elsaeed@paytabs.com>
2023-11-07 03:37:02 +0200

    update version

--- admin/language/en-gb/extension/payment/paytabs_souhoola.php
+++ admin/language/en-gb/extension/payment/paytabs_souhoola.php
@@ -3,4 +3,4 @@
 $_['heading_title'] = 'PayTabs - Souhoola';
 
 // Text
-$_['text_paytabs_souhoola'] = '<img src="view/image/payment/paytabs/tabby.svg" alt="PayTabs - Tabby" title="PayTabs - Tabby" style="max-width: 90px; max-height: 70px;" />';
+$_['text_paytabs_souhoola'] = '<img src="view/image/payment/paytabs/souhoola.png" alt="PayTabs - Souhoola" title="PayTabs - Souhoola" style="max-width: 90px; max-height: 70px;" />';
