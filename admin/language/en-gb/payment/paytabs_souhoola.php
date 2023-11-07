306a528aa5d16b8c5bb741daa5f4f568b5e41847
Walaa-Elsaeed-93 <w.elsaeed@paytabs.com>
2023-11-07 03:32:42 +0200

    souhoola payment method

---
+++ admin/language/en-gb/payment/paytabs_souhoola.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Souhoola';
+
+// Text
+$_images_url = HTTP_CATALOG . 'extension/paytabs/admin/view/image/';
+$_['text_paytabs_souhoola'] = '<img src="' . $_images_url . 'souhoola.png" alt="PayTabs - Souhoola" title="PayTabs - Souhoola" style="max-width: 90px; max-height: 70px;" />';
