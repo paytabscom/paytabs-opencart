ecc2916cf623fc267f4f8279b0174f9b469f9d37
Mohamed Hamed <mohamed.hamed@paytabs.com>
2023-11-23 15:27:54 +0200

    update set theme config

--- system/library/paytabs_api.php
+++ system/library/paytabs_api.php
@@ -636,6 +636,7 @@ abstract class PaytabsCatalogController extends \Opencart\System\Engine\Controll
         $hide_shipping = (bool) $this->config->get(PaytabsAdapter::_key('hide_shipping', $this->_code));
         $iframe = (bool) $this->config->get(PaytabsAdapter::_key('iframe', $this->_code));
         $allow_associated_methods = (bool) $this->config->get(PaytabsAdapter::_key('allow_associated_methods', $this->_code));
+        $theme_config_id = $this->config->get(PaytabsAdapter::_key('config_id', $this->controller->_code));
 
         //
 
@@ -676,6 +677,7 @@ abstract class PaytabsCatalogController extends \Opencart\System\Engine\Controll
             ->set07URLs($return_url, $callback_url)
             ->set08Lang($lang_code)
             ->set09Framed($iframe, 'top')
+            ->set11ThemeConfigId($theme_config_id)
             ->set99PluginInfo('OpenCart', VERSION, PAYTABS_PAYPAGE_VERSION);
 
         if ($this->_code === 'valu') {
