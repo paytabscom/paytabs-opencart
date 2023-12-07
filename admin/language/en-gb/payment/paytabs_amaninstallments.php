1526fe0f9bac12dcfadbca42b4b642fb704dd23a
Mohamed Hamed <mohamed.hamed@paytabs.com>
2023-12-07 10:02:13 +0200

    add aman installments to v4

---
+++ admin/language/en-gb/payment/paytabs_amaninstallments.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Aman';
+
+// Text
+$_images_url = HTTP_CATALOG . 'extension/paytabs/admin/view/image/';
+$_['text_paytabs_amaninstallments'] = '<img src="' . $_images_url . 'amaninstallments.svg" alt="PayTabs - Aman Installments" title="PayTabs - Aman Installments" style="max-width: 90px; max-height: 70px;" />';
