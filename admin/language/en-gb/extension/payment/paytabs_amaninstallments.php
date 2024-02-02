a1ff8525a406b83cdc4adc9e282c48b6f7e508a4
Mohamed-Hamed999 <148044093+Mohamed-Hamed999@users.noreply.github.com>
2024-02-02 08:17:08 +0200

    3.x Aman installments payment method (#46)
    * add Aman Installments payment method

---
+++ admin/language/en-gb/extension/payment/paytabs_amaninstallments.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Aman Installments';
+
+// Text
+$_['text_paytabs_amaninstallments'] = '<img src="view/image/payment/paytabs/amaninstallments.svg" alt="PayTabs - Aman Installments" title="PayTabs - Aman Installments" style="max-width: 90px; max-height: 70px;" />';
