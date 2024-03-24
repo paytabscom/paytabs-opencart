12bf675369422e3d05a9253e872f85ee705be218
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-03-24 17:04:01 +0200

    remove fawry

--- admin/language/en-gb/extension/payment/paytabs_fawry.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'PayTabs - @Fawry';
-
-// Text
-$_['text_paytabs_fawry'] = '<img src="view/image/payment/paytabs/fawry.png" alt="PayTabs - @Fawry" title="PayTabs - @Fawry" style="max-width: 90px; max-height: 70px;" />';
