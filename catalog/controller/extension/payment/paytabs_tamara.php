f05353ceeef1b194916308c5e9b5ad658251cff3
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-07-21 16:41:53 +0300

    tamara method

---
+++ catalog/controller/extension/payment/paytabs_tamara.php
@@ -0,0 +1,45 @@
+<?php
+
+class ControllerExtensionPaymentPaytabsTamara extends Controller
+{
+	public $_code = 'tamara';
+
+	private $paytabsController;
+
+
+	public function init()
+	{
+		$this->load->library('paytabs_api');
+
+		$this->paytabsController = new PaytabsCatalogController($this);
+	}
+
+	public function index()
+	{
+		$this->init();
+
+		return $this->paytabsController->index($data);
+	}
+
+
+	public function confirm()
+	{
+		$this->init();
+		return $this->paytabsController->confirm($data);
+	}
+
+
+	public function callback()
+	{
+		$this->init();
+
+		$this->paytabsController->callback();
+	}
+
+	public function redirectAfterPayment()
+	{
+		$this->init();
+
+		$this->paytabsController->redirectAfterPayment();
+	}
+}
