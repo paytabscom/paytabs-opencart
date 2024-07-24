53aaf33a763b2afdb9b482b4b185cafbfdd1570c
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-07-24 16:11:16 +0300

    Tamara v3 (#53)
    * Tamara payment method

---
+++ admin/language/en-gb/extension/payment/paytabs_tamara.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Tamara';
+
+// Text
+$_['text_paytabs_tamara'] = '<img src="view/image/payment/paytabs/tamara.svg" alt="PayTabs - Tamara" title="PayTabs - Tamara" style="max-width: 90px; max-height: 70px;" />';
