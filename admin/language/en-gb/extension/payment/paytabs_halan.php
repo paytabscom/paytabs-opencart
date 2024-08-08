e397fd170b663f24193babbaaa6c354c3de1bc46
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-08-08 19:39:41 +0300

    halan payment method

---
+++ admin/language/en-gb/extension/payment/paytabs_halan.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Halan';
+
+// Text
+$_['text_paytabs_halan'] = '<img src="view/image/payment/paytabs/halan.svg" alt="PayTabs - Halan" title="PayTabs - Halan" style="max-width: 90px; max-height: 70px;" />';
