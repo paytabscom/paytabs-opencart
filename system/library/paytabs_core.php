da521915ff3ade34c77f17d168f9f6155339a99c
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-08-19 16:27:08 +0300

    Halan (#54)
    * Halan payment method

--- system/library/paytabs_core.php
+++ system/library/paytabs_core.php
@@ -4,11 +4,11 @@ namespace Opencart\System\Library;
 
 /**
  * PayTabs v2 PHP SDK
- * Version: 2.22.0
+ * Version: 2.23.0
  * PHP >= 7.0.0
  */
 
-define('PAYTABS_SDK_VERSION', '2.22.0');
+define('PAYTABS_SDK_VERSION', '2.23.0');
 
 define('PAYTABS_DEBUG_FILE_NAME', 'debug_paytabs.log');
 define('PAYTABS_DEBUG_SEVERITY', ['Info', 'Warning', 'Error']);
@@ -1433,6 +1433,7 @@ class PaytabsApi
         '25' => ['name' => 'souhoola', 'title' => 'PayTabs - Souhoola', 'currencies' => ['EGP'], 'groups' => [PaytabsApi::GROUP_IFRAME, PaytabsApi::GROUP_REFUND]],
         '26' => ['name' => 'amaninstallments', 'title' => 'PayTabs - Aman installments', 'currencies' => ['EGP'], 'groups' => [PaytabsApi::GROUP_IFRAME, PaytabsApi::GROUP_REFUND]],
         '27' => ['name' => 'tamara', 'title' => 'PayTabs - Tamara', 'currencies' => ['AED', 'SAR'], 'groups' => [PaytabsApi::GROUP_IFRAME, PaytabsApi::GROUP_REFUND]],
+        '28' => ['name' => 'halan', 'title' => 'PayTabs - Halan', 'currencies' => ['EGP'], 'groups' => [PaytabsApi::GROUP_IFRAME]],
 
     ];
 
