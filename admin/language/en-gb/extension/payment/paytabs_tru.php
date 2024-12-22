d2419c23632148dfce17dffd283395eeba15916c
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-12-22 14:25:42 +0200

    add tru

---
+++ admin/language/en-gb/extension/payment/paytabs_tru.php
@@ -0,0 +1,5 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Tru';
+// Text
+$_['text_paytabs_tru'] = '<img src="view/image/payment/paytabs/tru.svg" alt="PayTabs - Halan" title="PayTabs - Tru" style="max-width: 90px; max-height: 70px;" />';
