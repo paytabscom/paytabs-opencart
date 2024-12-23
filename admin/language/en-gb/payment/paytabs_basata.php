f4eab8ad03cfc8e7ed4e250a6b3b88332d6b253c
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2024-12-23 15:20:04 +0200

    Basata (#59)
    * Basata payment method

---
+++ admin/language/en-gb/payment/paytabs_basata.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'PayTabs - Basata';
+
+// Text
+$_images_url = HTTP_CATALOG . 'extension/paytabs/admin/view/image/';
+$_['text_paytabs_basata'] = '<img src="' . $_images_url . 'basata.svg" alt="PayTabs - Basata" title="PayTabs - Basata" style="max-width: 90px; max-height: 70px;" />';
--- admin/language/en-gb/payment/paytabs_strings.php
+++ admin/language/en-gb/payment/paytabs_strings.php
@@ -25,6 +25,7 @@ $_['souhoola_heading_title']        = 'PayTabs - Souhoola';
 $_['amaninstallments_heading_title'] = 'PayTabs - Aman Installments';
 $_['tamara_heading_title']           = 'PayTabs - Tamara';
 $_['halan_heading_title']            = 'PayTabs - Halan';
+$_['basata_heading_title']           = 'PayTabs - Basata';
 
 // Text
 $_['text_extension']                = 'Extensions';
