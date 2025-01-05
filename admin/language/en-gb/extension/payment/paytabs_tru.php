aa33d66ce39912228ae08fbd6f39b67215995840
Walaa-Elsaeed-93 <w.elsaeed@paytabs.com>
2025-01-05 16:41:18 +0000

    add tru payment method

---
+++ admin/language/en-gb/extension/payment/paytabs_tru.php
@@ -0,0 +1,7 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'PayTabs - Tru';
+
+// Text
+$_['text_paytabs_tru'] = '<img src="view/image/payment/paytabs/tru.svg" alt="PayTabs - Tru" title="PayTabs - Tru" style="max-width: 90px; max-height: 70px;" />';
