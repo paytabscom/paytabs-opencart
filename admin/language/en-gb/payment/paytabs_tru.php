1f7a315a12d6267bab4c878ba56d646236ade81f
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2025-01-09 09:12:44 +0200

    Tru (#63)
    * TRU (Shahry) payment method

---
+++ admin/language/en-gb/payment/paytabs_tru.php
@@ -0,0 +1,9 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'PayTabs - TRU';
+
+// Text
+$_images_url = HTTP_CATALOG . 'extension/paytabs/admin/view/image/';
+
+$_['text_paytabs_tru'] = '<img src="' . $_images_url . 'tru.svg" alt="PayTabs - TRU" title="PayTabs - TRU" style="max-width: 90px; max-height: 70px;" />';
